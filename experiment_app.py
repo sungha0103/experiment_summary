62e89dff684764a919f4020431a1ea9cb77c4548
sungha0103 <sunghajung0103@gmail.com>
2026-07-06 12:53:17 +0900

    Change 'Hello World' to 'Goodbye World'

--- experiment_app.py
+++ experiment_app.py
@@ -81,7 +81,10 @@ with st.sidebar:
     file = st.file_uploader("CSV 파일 업로드", type="csv")
 
 if file is None:
-    st.info("실험 데이터는 복잡하게, 결과는 간단하게. \n사이드바에서 CSV 파일을 업로드해 주세요.")
+    st.info("""실험 데이터는 복잡하게, 결과는 간단하게.
+    
+    사이드바에서 CSV 파일을 업로드해 주세요.
+    """)
     st.stop()
 
 try:
